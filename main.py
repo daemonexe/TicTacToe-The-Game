4e0d60303e1a8cb156222cc9b0db67cbbd657907
Thanujan <k.thanujan.cs@gmail.com>
2023-02-07 22:42:02 +0530

    GUI Update
    Added GUI for Sngle/Multi- player options....

--- main.py
+++ main.py
@@ -25,6 +25,9 @@ def main():
     single_player_button_img = PhotoImage(file = 'main_menu_imgs/singleplayer_button.png')
     multi_player_button_img = PhotoImage(file= 'main_menu_imgs/multiplayer_image.png' )
     exit_button_img = PhotoImage(file= 'main_menu_imgs/exit_image.png')
+    singlePlayerImage = PhotoImage(file = 'single_player_imgs/single_player_background.png')
+    backButtonImage = PhotoImage(file= 'single_player_imgs/backButtonImage.png')
+    tile_image = PhotoImage(file = 'single_player_imgs/x_x.png')
 
     # Lables
     background_main_menu = Label(main_menu,image = main_menu_image, bd = 0, bg = 'black')
@@ -39,17 +42,83 @@ def main():
     exit_button.grid(row = 62,column = 53)
 
     # Button Functions :
-
-    def pressed_on_singleplayer():
-        pass
-
-    def pressed_on_multiplayer():
-        pass
-
     def pressed_on_exit(exit):
         window.destroy()
 
+    def single_player_pressed(a):
+        switch_page(single_player)
+
+    def multi_player_pressed(a):
+        switch_page(multi_player)
+
+    # ----------------------- SINGLE PLAYER -------------------------------
+    background_slngle_playeru = Label(single_player,image = singlePlayerImage, bd = 0, bg = 'black')
+    backButton = Label(single_player, image = backButtonImage,bd = 0,bg = 'black')
+
+    tile1 = Label(single_player, image= tile_image,bd= 0)
+    tile2 = Label(single_player, image= tile_image,bd= 0)
+    tile3 = Label(single_player, image= tile_image,bd= 0)
+    tile4 = Label(single_player, image= tile_image,bd= 0)
+    tile5 = Label(single_player, image= tile_image,bd= 0)
+    tile6 = Label(single_player, image= tile_image,bd= 0)
+    tile7 = Label(single_player, image= tile_image,bd= 0)
+    tile8 = Label(single_player, image= tile_image,bd= 0)
+    tile9 = Label(single_player, image= tile_image,bd= 0)
+
+
+    # functions
+    def backButtonPressed(back):
+        switch_page(main_menu)
+
+    background_slngle_playeru.grid(rowspan=100,columnspan=100)
+    backButton.grid(row = 0,column=0)
+    backButton.bind('<Button-1>',backButtonPressed)
+
+    tile1.grid(row=9, column=13, columnspan=20)
+    tile2.grid(row=9, column=34, columnspan=25)
+    tile3.grid(row=9, column=52, columnspan=35)
+    tile4.grid(row=10, column=13, columnspan=20, rowspan=38)
+    tile5.grid(row=10, column=34, columnspan=25, rowspan=38)
+    tile6.grid(row=10, column=52, columnspan=35, rowspan=38)
+    tile7.grid(row=44, column=13, columnspan=20, rowspan=42)
+    tile8.grid(row=44, column=34, columnspan=25, rowspan=42)
+    tile9.grid(row=44, column=52, columnspan=35, rowspan=42)
+
+    # ----------------- MULTIPLAYER -----------------
+
+    background_multiplayer = Label(multi_player, image=singlePlayerImage, bd=0, bg='black')
+    backButton = Label(multi_player, image=backButtonImage, bd=0, bg='black')
+
+    _tile1 = Label(multi_player, image=tile_image, bd=0)
+    _tile2 = Label(multi_player, image=tile_image, bd=0)
+    _tile3 = Label(multi_player, image=tile_image, bd=0)
+    _tile4 = Label(multi_player, image=tile_image, bd=0)
+    _tile5 = Label(multi_player, image=tile_image, bd=0)
+    _tile6 = Label(multi_player, image=tile_image, bd=0)
+    _tile7 = Label(multi_player, image=tile_image, bd=0)
+    _tile8 = Label(multi_player, image=tile_image, bd=0)
+    _tile9 = Label(multi_player, image=tile_image, bd=0)
+
+    # functions
+    def backButtonPressed(back):
+        switch_page(main_menu)
+
+    background_multiplayer.grid(rowspan=100, columnspan=100)
+    backButton.grid(row=0, column=0)
+    _tile1.grid(row=9, column=13, columnspan=20)
+    _tile2.grid(row=9, column=34, columnspan=25)
+    _tile3.grid(row=9, column=52, columnspan=35)
+    _tile4.grid(row=10, column=13, columnspan=20, rowspan=38)
+    _tile5.grid(row=10, column=34, columnspan=25, rowspan=38)
+    _tile6.grid(row=10, column=52, columnspan=35, rowspan=38)
+    _tile7.grid(row=44, column=13, columnspan=20, rowspan=42)
+    _tile8.grid(row=44, column=34, columnspan=25, rowspan=42)
+    _tile9.grid(row=44, column=52, columnspan=35, rowspan=42)
+
     exit_button.bind("<Button-1>", pressed_on_exit)
+    single_player_button.bind("<Button-1>", single_player_pressed)
+    multi_player_button.bind("<Button-1>", multi_player_pressed)
+    backButton.bind('<Button-1>',backButtonPressed)
 
     window.geometry('722x600')
     window.mainloop()
